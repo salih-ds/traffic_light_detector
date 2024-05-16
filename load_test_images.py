a0b977618bda193f7d0364ceb353aeabadc0869a
Руслан Салих <ruslansalih@MacBook-Pro-Ruslan.local>
2024-05-16 22:10:52 +0300

    template

---
+++ load_test_images.py
@@ -0,0 +1,7 @@
+import utils.utils as ut
+
+def main():
+    ut.load_test_images()
+
+if __name__ == '__main__':
+    main()
---
+++ utils/utils.py
@@ -0,0 +1,15 @@
+import gdown
+import zipfile
+import os
+
+def gdownload(output, url):
+  gdown.download(url=url, output=output, fuzzy=True)
+
+def load_test_images(dir='images', file='test_image.zip', 
+                     gdive='https://drive.google.com/file/d/12CJkx7MeMXxYYV2RprTM-uMRBcNV2OqU/view?usp=sharing'):
+    
+    gdownload(file, gdive)
+    with zipfile.ZipFile(file, 'r') as zip_ref:
+        zip_ref.extractall(dir)
+    
+    os.remove(file)
